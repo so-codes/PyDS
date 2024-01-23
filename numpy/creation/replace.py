9b7dc681c4d69267be73c4aaf2b5249fb11cbcf8
Kurizu <83665497+crizmo@users.noreply.github.com>
2024-01-23 11:54:21 +0530

    Update replace.py

--- numpy/creation/replace.py
+++ numpy/creation/replace.py
@@ -1,6 +1,17 @@
-a = [1, 2, 3, 4, 5, 6,7,8,9]
-for i, n in enumerate(a):
-  if n % 2 != 0:
-     a[i] = -1
+import numpy as np
 
-print(a)
+# 1 D 
+a = np.array([1, 2, 3, 4, 5, 6,7,8,9])
+
+print("1D \n",np.where(a %2 != 0, -1, a), "\n\n")
+
+# 2 D
+b = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("2D \n",np.where(b %2 != 0, -1, b), "\n\n")
+
+# 3 D
+c = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print("3D \n", np.where(c %2 != 0, -1, c), "\n")
+
